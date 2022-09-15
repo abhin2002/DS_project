f05add3a46897ba52d301657a45e121401e4ee12
AkuOO4 <akshayks2004@gmail.com>
2022-09-15 23:16:10 +0530

    aku

--- streamlit-sales-dashboard-main/app.py
+++ streamlit-sales-dashboard-main/app.py
@@ -8,6 +8,7 @@
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
+from sub import top_10
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
